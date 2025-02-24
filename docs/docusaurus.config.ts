7dade9b3c4150c537a8e426e29bf826e01539851
igorcaldeira <igorcaldeira@Macmini.lan>
2025-02-23 23:47:10 -0300

    docs

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -11,12 +11,12 @@ const config: Config = {
   url: "https://campusvirtualfiocruz.github.io/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/painel-esus/",
+  baseUrl: "/painel-esus-chromatic-test/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "campusvirtualfiocruz", // Usually your GitHub org/user name.
-  projectName: "painel-esus", // Usually your repo name.
+  projectName: "painel-esus-chromatic-test", // Usually your repo name.
   trailingSlash: false,
 
   onBrokenLinks: "throw",
